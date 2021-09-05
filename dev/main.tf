0d1373b04112fa36a1193012f0168cf26d0fbc88
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-09-05 16:40:02 +0700

    feat: use devinfra module v0.0.1

--- dev/main.tf
+++ dev/main.tf
@@ -1,9 +1,28 @@
-terraform {}
-
-locals {
-  hello = "Hello, world!"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.82.0"
+    }
+  }
 }
 
-output "hello" {
-  value = local.hello
+locals {
+  credentials = (
+    var.credentials != "" ? var.credentials : file(".env/service-account.json")
+  )
+}
+
+provider "google" {
+  credentials = local.credentials
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+module "devinfra" {
+  source  = "app.terraform.io/deuterionnet/devinfra/deuterionnet"
+  version = "0.0.1"
+  project = var.project
 }
---
+++ dev/outputs.tf
@@ -0,0 +1,7 @@
+output "instance_network_ip" {
+  value = module.devinfra.instance_network_ip
+}
+
+output "instance_nat_ip" {
+  value = module.devinfra.instance_nat_ip
+}

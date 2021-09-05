14d530f3ef67b0e28a1730f14ffd251579feba78
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-09-05 19:43:03 +0700

    refactor: update provider block

--- dev/main.tf
+++ dev/main.tf
@@ -1,31 +1,14 @@
 terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 3.82.0"
-    }
-  }
-}
-
-locals {
-  credentials = (
-    var.credentials != "" ? var.credentials : file(".env/service-account.json")
-  )
-}
-
-provider "google" {
-  credentials = local.credentials
-
-  project = var.project
-  region  = var.region
-  zone    = var.zone
+  required_version = ">= 1.0.5"
 }
 
 # Use private module
 # name: terraform-deuterionnet-devinfra
-# version: 0.0.1
+# version: 0.0.3
 module "devinfra" {
-  source  = "app.terraform.io/deuterionnet/devinfra/deuterionnet"
-  version = "0.0.1"
-  project = var.project
+  source         = "app.terraform.io/deuterionnet/devinfra/deuterionnet"
+  version        = "0.0.3"
+  gcp_project_id = var.gcp_project_id
+  gcp_region     = var.gcp_region
+  gcp_zone       = var.gcp_zone
 }

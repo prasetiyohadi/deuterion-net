09b4670435c12c81ffa1b93fd4c01599ccf8fc97
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-09-05 16:46:19 +0700

    refactor: add comment for module devinfra

--- dev/main.tf
+++ dev/main.tf
@@ -21,6 +21,9 @@ provider "google" {
   zone    = var.zone
 }
 
+# Use private module
+# name: terraform-deuterionnet-devinfra
+# version: 0.0.1
 module "devinfra" {
   source  = "app.terraform.io/deuterionnet/devinfra/deuterionnet"
   version = "0.0.1"

56ee3cffc5b65a8a39867a519e5efde295c6d276
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-09-05 19:42:42 +0700

    refactor: update output entries description

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,7 +1,9 @@
-output "instance_network_ip" {
-  value = module.devinfra.instance_network_ip
+output "instance_nat_ip" {
+  description = "The public IP address of the compute instance."
+  value       = module.devinfra.instance_nat_ip
 }
 
-output "instance_nat_ip" {
-  value = module.devinfra.instance_nat_ip
+output "instance_network_ip" {
+  description = "The internal IP address of the compute instance."
+  value       = module.devinfra.instance_network_ip
 }
